73d5b74d1c9d6dd62de8372db2b583146c88281b
rafaelvaloto <rafaelvaloto@gmail.com>
2026-04-09 01:18:48 -0300

    Adjust `NRComponent` and `NRRigScales` defaults, refine pelvis, leg, and foot IK transformations, and remove redundant debug logs.

--- Source/UnrealNeuraRig/Private/API/v1/NRComponent.cpp
+++ Source/UnrealNeuraRig/Private/API/v1/NRComponent.cpp
@@ -190,49 +190,44 @@ void UNRComponent::UpdateIK(USkeletalMeshComponent* CharacterMesh, const TArray<
 		return FQuat(p[0], p[1], p[2], p[3]).Rotator();
 	};
 
-	// 1. Pelvis (Offset 0, Size 7: vec3 + Quat)
+	// Pelvis (Offset 0, Size 7: vec3 + Quat)
 	FVector PelvisPos = FVector(PacketRecive[0], PacketRecive[1], PacketRecive[2]);
-	PelvisPos.X *= RigScales.MaxPelvisSwayY;
+	PelvisPos.X *= RigScales.MaxPelvisForwardX;
 	PelvisPos.Y *= RigScales.MaxPelvisSwayY;
 	PelvisPos.Z *= RigScales.MaxPelvisDropZ;
 	OutPelvis_Pos = FMath::VInterpTo(OutPelvis_Pos, PelvisPos, T_intpl, S_intpl);
 	
 	FRotator PelvisRot = GetRot(&PacketRecive[3]);
 	OutPelvis_Rot = FMath::RInterpTo(OutPelvis_Rot, PelvisRot, T_intpl, S_intpl);
-
-	// 2. Foot IK (Offset 7, Size 14: 2x vec3 + Quat)
-	FVector FootScale = FVector(RigScales.MaxFootStrideX, RigScales.MaxFootWidthY, RigScales.MaxFootHeightZ);
+	
+	// Foot IK (Offset 7, Size 14: 2x vec3 + Quat)
+	FVector FootScale = FVector(0.0, 65.0,  -92.0);
 	
 	// Foot R (Offset 7)
 	FVector LocalPosR = FVector(PacketRecive[7], PacketRecive[8], PacketRecive[9]) * FootScale;
-	UE_LOG(LogTemp, Log, TEXT("R: LocalPosR=%f %f %f"), LocalPosR.X, LocalPosR.Y, LocalPosR.Z);
-	
 	OutFootR_Pos = FMath::VInterpTo(OutFootR_Pos, LocalPosR, T_intpl, S_intpl);
 	OutFootR_Rot = FMath::RInterpTo(OutFootR_Rot, GetRot(&PacketRecive[10]), T_intpl, S_intpl);
 
 	// Foot L (Offset 14)
 	FVector LocalPosL = FVector(PacketRecive[14], PacketRecive[15], PacketRecive[16]) * FootScale;
-	OutFootL_Pos = FMath::VInterpTo(OutFootL_Pos, -LocalPosL, T_intpl, S_intpl);
+	OutFootL_Pos = FMath::VInterpTo(OutFootL_Pos, LocalPosL, T_intpl, S_intpl);
 	OutFootL_Rot = FMath::RInterpTo(OutFootL_Rot, GetRot(&PacketRecive[17]), T_intpl, S_intpl);
-	UE_LOG(LogTemp, Log, TEXT("R: LocalPosL=%f %f %f"), LocalPosR.X, LocalPosR.Y, LocalPosR.Z);
-
-	// 3. Legs IK (Offset 21, Size 28: 4x vec3 + Quat)
+	
 	// Leg R Calf (Offset 21 + 0*7)
 	OutCalfR_Rot = FMath::RInterpTo(OutCalfR_Rot, GetRot(&PacketRecive[24]), T_intpl, S_intpl);
 	// Leg L Calf (Offset 21 + 1*7)
 	OutCalfL_Rot = FMath::RInterpTo(OutCalfL_Rot, GetRot(&PacketRecive[31]), T_intpl, S_intpl);
+	
 	// Leg R Thigh (Offset 21 + 2*7)
-	UE_LOG(LogTemp, Warning, TEXT("OutThighR_Rot=%s"), *OutThighR_Rot.ToString());
-	UE_LOG(LogTemp, Warning, TEXT("OutThighL_Rot=%s"), *OutThighL_Rot.ToString());
 	OutThighR_Rot = FMath::RInterpTo(OutThighR_Rot, GetRot(&PacketRecive[38]), T_intpl, S_intpl);
 	// Leg L Thigh (Offset 21 + 3*7)
 	OutThighL_Rot = FMath::RInterpTo(OutThighL_Rot, GetRot(&PacketRecive[45]), T_intpl, S_intpl);
 
-	// Debug visualization (restored and adapted)
+	// Debug visualization
 	if (CharacterMesh)
 	{
-		FVector WorldR = GetOwner()->GetActorTransform().TransformPosition(FVector(-OutFootR_Pos.Y, OutFootR_Pos.Z, -OutFootR_Pos.X));
-		FVector WorldL = GetOwner()->GetActorTransform().TransformPosition(FVector(OutFootL_Pos.Y, OutFootL_Pos.Z, OutFootL_Pos.X));
+		FVector WorldR = GetOwner()->GetActorTransform().TransformPosition(FVector(OutFootR_Pos.Y, OutFootR_Pos.X, OutFootR_Pos.Z));
+		FVector WorldL = GetOwner()->GetActorTransform().TransformPosition(FVector(OutFootL_Pos.Y, OutFootL_Pos.X, OutFootL_Pos.Z));
 		
 		WorldR.Y = CharacterMesh->GetBoneLocation("foot_r").Y;
 		WorldL.Y = CharacterMesh->GetBoneLocation("foot_l").Y;
